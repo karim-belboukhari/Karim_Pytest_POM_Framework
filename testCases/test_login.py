ed788212f93df415f5c431cf850ce160899caa65
karim <belboukharikarimx@gmail.com>
2024-08-28 02:45:04 +0100

    update the pross of request

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -6,14 +6,15 @@ from Utilites.customlogger import Logenerator
 import pytest
 import os
 
+
 class Test_Login:
-    screen = os.path.join(os.path.dirname(__file__), '..', 'Screenshots', 'myimage.png')
+    
     email=Readconfig.getemail()
     password=Readconfig.getpassword()
     logger = Logenerator.log_gen()
      
     @pytest.mark.Sanity   
-    def test_log(self,setup):
+    def test_valid_login(self,setup, request):
         
         self.logger.info("***************verifying the logging page****************")
         
@@ -22,12 +23,20 @@ class Test_Login:
         login.set_username(self.email)
         login.set_password(self.password)
         login.click_log()
-        if driver.title == "OrangeHRM":
+        if driver.title == "OrangeHRsM":
             assert True
             self.logger.info("***************logging page passed****************")
         else:
-            driver.save_screenshot(self.screen)
+            test_name = request.node.name
+            screen = os.path.join(os.path.dirname(__file__), '..', 'Screenshots', f'{test_name}.png')
+            driver.save_screenshot(screen)
             self.logger.info("***************logging page failed****************")
             assert False
-            
-            
+    def test_invalid_login(self,setup):
+        pass       
+    def test_login_with_empty_fields(self,setup):
+        pass 
+    def test_login_with_max_length_username(self,setup):
+        pass 
+    def test_existing_functionality(self,setup):
+        pass        
