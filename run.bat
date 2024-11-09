96760a3388e566c9cd7632a1d154c553263d6e25
karim <belboukharikarimx@gmail.com>
2024-11-09 20:03:35 +0100

    update the run file

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
-pytest -v --alluredir=report/allure-results
+pytest -v -n 3 --alluredir=report/allure-results
 allure generate report/allure-results -o report/allure-report --clean
 allure open report/allure-report
